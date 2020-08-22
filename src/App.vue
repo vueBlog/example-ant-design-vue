66e097e07dbcb7f29598178dd2cd831c4e01dd01
fxss <723107234@qq.com>
2020-08-22 14:29:32 +0800

    :heavy_plus_sign: 添加ant-design-vue

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
+    <a-button type="primary">Button</a-button>
     <router-view/>
   </div>
 </template>
